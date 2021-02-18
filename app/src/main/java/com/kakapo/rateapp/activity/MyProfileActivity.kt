6439da64cbc18df5126b1446499949e3a4ee9047
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-02-18 15:26:41 +0700

    chose image from gallery

--- app/src/main/java/com/kakapo/rateapp/activity/MyProfileActivity.kt
+++ app/src/main/java/com/kakapo/rateapp/activity/MyProfileActivity.kt
@@ -1,19 +1,82 @@
 package com.kakapo.rateapp.activity
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.kakapo.rateapp.R
 import com.kakapo.rateapp.firestore.FireStoreClass
 import com.kakapo.rateapp.model.User
 import kotlinx.android.synthetic.main.activity_my_profile.*
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
+
+    companion object{
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
         setupActionBar()
 
         FireStoreClass().loadUserData(this)
+        setupChangeImageFromStorage()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                Toast.makeText(
+                    this@MyProfileActivity,
+                    "Please give Storage permission",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (
+            resultCode == Activity.RESULT_OK &&
+            requestCode == PICK_IMAGE_REQUEST_CODE &&
+            data!!.data != null
+        ){
+            mSelectedImageFileUri = data.data
+
+            try{
+                Glide.with(this@MyProfileActivity)
+                    .load(mSelectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(iv_profile_user_image)
+            }catch (e: IOException){
+                Log.e("Error MyProfileActivity",
+                    "try to populate image failure onActivity Result",
+                    e
+                )
+            }
+        }
     }
 
     private fun setupActionBar(){
@@ -31,12 +94,36 @@ class MyProfileActivity : BaseActivity() {
 
     }
 
+    private fun setupChangeImageFromStorage(){
+        iv_profile_user_image.setOnClickListener {
+            if(
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            }else{
+                ActivityCompat.requestPermissions(
+                    this@MyProfileActivity,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    private fun showImageChooser(){
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
     fun setUserDataInUi(user: User){
         Glide.with(this@MyProfileActivity)
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(iv_user_image)
+            .into(iv_profile_user_image)
 
         et_name.setText(user.name)
         et_email.setText(user.email)
